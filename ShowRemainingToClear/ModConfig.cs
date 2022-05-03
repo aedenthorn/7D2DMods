8e4cbaab48c6f2b64a2c762a6ccf26e05135cd40
aedenthorn <aedenthorn1@gmail.com>
2022-05-03 19:53:34 -0400

    show remaining

---
+++ ShowRemainingToClear/ModConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing;
+using System.Numerics;
+using UnityEngine;
+
+namespace ShowRemainingToClear
+{
+    public class ModConfig
+    {
+        public bool modEnabled = true;
+        public bool isDebug = true;
+        public string remainingText = "{0} ({1} left)";
+    }
+}
---
+++ ShowRemainingToClear/ShowRemainingToClear.cs
@@ -0,0 +1,76 @@
+﻿using HarmonyLib;
+using Newtonsoft.Json;
+using Platform;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Threading;
+using UnityEngine;
+
+namespace ShowRemainingToClear
+{
+    public class ShowRemainingToClear : IModApi
+    {
+        private static ShowRemainingToClear context;
+        private static Mod mod;
+        private static Dictionary<Vector3i, TileEntitySecureLootContainer> storageDict = new Dictionary<Vector3i, TileEntitySecureLootContainer>();
+        public static ModConfig config;
+        public void InitMod(Mod modInstance)
+        {
+            LoadConfig();
+
+            context = this;
+            mod = modInstance;
+            Harmony harmony = new Harmony(GetType().ToString());
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+        public void LoadConfig()
+        {
+            var path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "config.json");
+            if (!File.Exists(path))
+            {
+                config = new ModConfig();
+            }
+            else
+            {
+                config = JsonConvert.DeserializeObject<ModConfig>(File.ReadAllText(path));
+            }
+            File.WriteAllText(path, JsonConvert.SerializeObject(config, Formatting.Indented));
+        }
+        public static void Dbgl(object str, bool prefix = true)
+        {
+            if (config.isDebug)
+                Debug.Log((prefix ? mod.ModInfo.Name.Value + " " : "") + str);
+        }
+
+        [HarmonyPatch(typeof(XUiC_QuestTrackerObjectiveEntry), nameof(XUiC_QuestTrackerObjectiveEntry.GetBindingValue))]
+        public static class XUiC_QuestObjectiveEntry_GetBindingValue_Patch
+        {
+            public static bool Prefix(XUiC_QuestTrackerObjectiveEntry __instance, ref string value, string bindingName)
+            {
+                if (!config.modEnabled || !(__instance.Objective is ObjectiveClearSleepers) || bindingName != "objectivedescription")
+                    return true;
+
+                //var spawnPointList = AccessTools.Field(typeof(SleeperVolume), "spawnPointList");
+
+                int left = 0;
+                foreach (var sed in QuestEventManager.Current.SleeperVolumeUpdateDictionary.Values)
+                {
+                    if (sed.EntityList.Exists(e => (GameManager.Instance.World.GetEntity(e) as EntityPlayer) is EntityPlayerLocal))
+                    {
+                        left = sed.SleeperVolumes.Count();
+                        break;
+                    }
+                }
+                if (left <= 0)
+                    return true;
+
+                value = string.Format(config.remainingText, __instance.Objective.Description, left);
+                return false;
+            }
+        }
+    }
+}
